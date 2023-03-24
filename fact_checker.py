148c113e7825c953a242160b278e53827973324e
ivanmarinoff <marinoffivan@gmail.com>
2023-03-24 18:08:08 +0200

    Added token usage tracking

--- fact_checker.py
+++ fact_checker.py
@@ -1,12 +1,17 @@
-from langchain.llms import OpenAI, openai
+from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import SimpleSequentialChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.callbacks import get_openai_callback
 import sys
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 os.environ.get("OPENAI_API_KEY")
+
 
 def fact_check(question):
     llm = OpenAI(temperature=0.7)
@@ -31,16 +36,22 @@ def fact_check(question):
     prompt_template = PromptTemplate(input_variables=["facts"], template=template)
     answer_chain = LLMChain(llm=llm, prompt=prompt_template)
 
-    overall_chain = SimpleSequentialChain(chains=[question_chain, assumptions_chain, fact_checker_chain, answer_chain], verbose=True)
+    overall_chain = SimpleSequentialChain(chains=[question_chain, assumptions_chain, fact_checker_chain, answer_chain],
+                                          verbose=True)
 
     return overall_chain.run(question)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     if len(sys.argv) > 1:
-        question = sys.argv[1]
+        question = input(sys.argv[1])  # added input()
     else:
         question = f"Your question is: {input()}\n"
     print(question)
     answer = fact_check(question)
     print(answer)
 
+    llm = OpenAI(model_name="text-davinci-002", n=2, best_of=2)
+    with get_openai_callback() as cb:
+        result = llm(question)
+        print(f"Token Usage Tracking: {cb.total_tokens}")
